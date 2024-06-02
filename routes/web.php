0e751906c382bfdd7a4579e85fcb7f5d20929c9d
aymane_B <aymanefast@gmail.com>
2024-06-02 11:45:26 +0100

    migrations and controller setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('tasks', TaskController::class);
